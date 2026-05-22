d820923cbe7ecd0e09493537bc73ca977129b8ff
Tathagata Nath <nath.riju53@gmail.com>
2026-05-23 02:57:16 +0530

    Update app.py

--- app.py
+++ app.py
@@ -20,7 +20,7 @@ st.set_page_config(
 )
 
 st.title("📈 Tathagata your Stock Market Advisor")
-st.markdown("Analyze Indian and USA stocks using AI agents.")
+st.markdown("Analyze Indian and USA stocks using AI agents.This app is build for learning purpose.")
 
 
 # =========================================
