4a05c6b424f013e113c205690e1166c80a1ebb03
Tathagata Nath <nath.riju53@gmail.com>
2026-05-26 20:33:29 +0530

    Update app.py

--- app.py
+++ app.py
@@ -42,7 +42,7 @@ st.markdown(
 
 # Set your Groq API key here
 #os.environ["GROQ_API_KEY"] = "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"
-os.environ["GROQ_API_KEY"] = "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"
+os.environ["GROQ_API_KEY"] = "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"
 
 # Finnhub API
 FINNHUB_API_KEY = "d87h1n1r01qmhakfpangd87h1n1r01qmhakfpao0"
